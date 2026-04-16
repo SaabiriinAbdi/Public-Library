84f74564c8d3f13d8a95e98ca70892c40359054b
Saabiriin Abdi <saabka46@gmail.com>
2026-04-16 10:25:14 -0500

    commented out the mockData since there isnt any

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -5,7 +5,7 @@ import { Button } from './ui/button';
 import { Badge } from './ui/badge';
 import { Input } from './ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
-import { EVENTS_DATA, Event, BRANCHES } from '../data/mockData';
+//import { EVENTS_DATA, Event, BRANCHES } from '../data/mockData';
 import { useAuth } from '../context/AuthContext';
 import { toast } from 'sonner';
 
