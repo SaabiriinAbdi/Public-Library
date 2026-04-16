e57db978a9c60e529912dcff0e0fb46f7260f36e
Saabiriin Abdi <saabka46@gmail.com>
2026-04-16 11:28:56 -0500

    Clean project, remove backend from frontend, fix dist

--- src/app/components/backend/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const db = await mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-});
